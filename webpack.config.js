3e7b80014635b4855951c62d99dcf5ea15fcac81
Aye Daniel A <aadaniel108@gmail.com>
2021-07-30 12:30:59 +0100

    re-configured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: path.resolve(__dirname, './src/index.js'),
+  },
   output: {
+    path: path.resolve(__dirname, './dist'),
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
