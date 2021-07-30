3bdcda7a8e1fa2b62d5e38ab570277ddecd6dfc0
Aye Daniel A <aadaniel108@gmail.com>
2021-07-30 13:20:27 +0100

    website compilation

---
+++ src/website.js
@@ -0,0 +1,109 @@
+import './style.css';
+import loadHome from './home';
+import loadMenu from './menu';
+import loadContact from './contact';
+
+function setActiveButton(button) {
+  const buttons = document.querySelectorAll('.button-nav');
+
+  buttons.forEach((button) => {
+    if (button !== this) {
+      button.classList.remove('active');
+    }
+  });
+
+  button.classList.add('active');
+}
+
+function createNav() {
+  const nav = document.createElement('nav');
+
+  const homeButton = document.createElement('button');
+  homeButton.classList.add('button-nav');
+  homeButton.textContent = 'Home';
+  homeButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
+    setActiveButton(homeButton);
+    loadHome();
+  });
+
+  const menuButton = document.createElement('button');
+  menuButton.classList.add('button-nav');
+  menuButton.textContent = 'Menu';
+  menuButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
+    setActiveButton(menuButton);
+    loadMenu();
+  });
+
+  const contactButton = document.createElement('button');
+  contactButton.classList.add('button-nav');
+  contactButton.textContent = 'Contact';
+  contactButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
+    setActiveButton(contactButton);
+    loadContact();
+  });
+
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(contactButton);
+
+  return nav;
+}
+
+function createHeader() {
+  const header = document.createElement('header');
+  header.classList.add('header');
+
+  const restaurantName = document.createElement('h1');
+  restaurantName.classList.add('restaurant-name');
+  restaurantName.textContent = 'The Place Restaurant, VI Lagos';
+
+  header.appendChild(restaurantName);
+
+  header.appendChild(createNav());
+
+  return header;
+}
+
+function createMain() {
+  const main = document.createElement('main');
+  main.classList.add('main');
+  main.setAttribute('id', 'main');
+  return main;
+}
+
+function createFooter() {
+  const footer = document.createElement('footer');
+  footer.classList.add('footer');
+
+  const copyright = document.createElement('p');
+  copyright.textContent = 'Copyright © July 2021 Aye Daniel';
+
+  const githubLink = document.createElement('a');
+  githubLink.href = 'https://github.com/alaska01';
+
+  const githubIcon = document.createElement('i');
+  githubIcon.classList.add('fab');
+  githubIcon.classList.add('fa-github');
+
+  githubLink.appendChild(githubIcon);
+  footer.appendChild(copyright);
+  footer.appendChild(githubLink);
+
+  return footer;
+}
+
+function initializeWebsite() {
+  const content = document.getElementById('content');
+
+  content.appendChild(createHeader());
+  content.appendChild(createMain());
+  content.appendChild(createFooter());
+
+  setActiveButton(document.querySelector('.button-nav'));
+  loadHome();
+}
+
+export default initializeWebsite;
